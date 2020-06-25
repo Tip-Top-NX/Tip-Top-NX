f7ab6bd47408918c3f28413f292f4c039d5d8e9f
Vidhi-D <vidhidhoka8@gmail.com>
2020-06-25 15:20:32 +0530

    search

--- frontend/Frontend/Routes/HomeStack.js
+++ frontend/Frontend/Routes/HomeStack.js
@@ -5,6 +5,7 @@ import Home from "../src/Catgories/Home/Home";
 import Header from "../src/Header";
 import Catelogue from "../src/Catgories/Catelogue";
 import Product from "../src/ProductDescription/ProductPage";
+import Search from '../src/SearchPage';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +22,7 @@ const HomeStack = () => {
       />
       <Stack.Screen name="Catelogue" component={Catelogue} />
       <Stack.Screen name="Product" component={Product} />
+      <Stack.Screen options={{headerShown:false}} name="Search" component={Search} />
     </Stack.Navigator>
   );
 };
