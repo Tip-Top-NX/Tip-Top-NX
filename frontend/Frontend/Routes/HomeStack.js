0cd888316a4e25c495207c7207e0b896fdecd1b0
Vishwesh Lingayat <vishwesh7231199@gmail.com>
2020-06-26 15:26:23 +0530

    Ignore

--- frontend/Frontend/Routes/HomeStack.js
+++ frontend/Frontend/Routes/HomeStack.js
@@ -5,26 +5,25 @@ import Home from "../src/Catgories/Home/Home";
 import Header from "../src/Header";
 import Catelogue from "../src/Catgories/Catelogue";
 import Product from "../src/ProductDescription/ProductPage";
+import Search from "../src/SearchPage";
 
 const Stack = createStackNavigator();
 
 const HomeStack = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTitle: () => <Header />,
-        headerShown: false,
-      }}
-    >
+    <Stack.Navigator>
       <Stack.Screen
         name="Categories"
         component={Home}
         options={{
           headerShown: true,
+          headerTitle: () => <Header />,
         }}
       />
       <Stack.Screen name="Catelogue" component={Catelogue} />
       <Stack.Screen name="Product" component={Product} />
+      <Stack.Screen name="Search" component ={Search} />
+      
     </Stack.Navigator>
   );
 };
--- frontend/Frontend/src/Header.js
+++ frontend/Frontend/src/Header.js
@@ -1,12 +1,32 @@
 /* eslint-disable */
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+
+import {
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  Image,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native'
+
 import { MaterialIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import Animated, { Easing } from 'react-native-reanimated'
+const { Value, timing } = Animated
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height/20
+
 const header = () => {
   const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <View style={styles.sideImage}>
@@ -22,6 +42,18 @@ const header = () => {
       <View style={styles.headingStyle}>
         <Text style={styles.textStyle}>TIP TOP NX</Text>
       </View>
+
+      <View>
+        <TouchableHighlight
+          activeOpacity={1}
+          underlayColor={"#ccd0d5"}
+          onPress={() => navigation.navigate("Search")}
+          style={styles.search_icon_box}
+        >
+          <Icon name="search" size={22} color="#000000" />
+        </TouchableHighlight>
+      </View>
+
       <View style={styles.sideImage}>
         <AntDesign name="shoppingcart" size={30} style={styles.materialIcon} />
       </View>
@@ -61,6 +93,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#000",
     textAlign: "center",
+  },
+  search_icon_box: {
+    width:40,
+    height: 40,
+    borderRadius: 40,
+    backgroundColor: '#e4e6eb',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 0,
+    marginLeft:20
   },
 });
 
