2a9eabf0dc740c3df40e6f6e3f51ae2b8a52fa73
Vishwesh Lingayat <vishwesh7231199@gmail.com>
2020-07-16 08:28:43 +0530

    Zero Order Page

--- frontend/Frontend/src/Orders/Orders.js
+++ frontend/Frontend/src/Orders/Orders.js
@@ -2,32 +2,41 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, FlatList } from "react-native";
 import { useSelector } from "react-redux";
+import Empty from "../Empty";
 import OrderCard from "./OrderCard";
 
 const Orders = ({ navigation }) => {
   const user = useSelector((state) => state.user);
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={user.orders}
-        vertical={true}
-        showsVerticalScrollIndicator={true}
-        keyExtractor={(item) => item._id.toString()}
-        renderItem={({ item }) => (
-          <OrderCard
-            _id={item._id}
-            amount={item.amount}
-            deliveryCharge={item.deliveryCharge}
-            status={item.status}
-            method={item.payment.method}
-            orderDate={item.orderDate}
-            contents={item.contents}
-            name={user.name}
-            address={user.address}
-            navigation={navigation}
-          />
-        )}
-      />
+      {user.orders.length === 0 ? (
+        <Empty
+          title="YOU DONT HAVE ANY ORDERS YET!"
+          desc="TO SELECT PRODUCTS TO ORDER"
+          navigation={props.navigation}
+        />
+        ) : (
+        <FlatList
+          data={user.orders}
+          vertical={true}
+          showsVerticalScrollIndicator={true}
+          keyExtractor={(item) => item._id.toString()}
+          renderItem={({ item }) => (
+            <OrderCard
+              _id={item._id}
+              amount={item.amount}
+              deliveryCharge={item.deliveryCharge}
+              status={item.status}
+              method={item.payment.method}
+              orderDate={item.orderDate}
+              contents={item.contents}
+              name={user.name}
+              address={user.address}
+              navigation={navigation}
+            />
+          )}
+        />
+      )}
     </SafeAreaView>
   );
 };
