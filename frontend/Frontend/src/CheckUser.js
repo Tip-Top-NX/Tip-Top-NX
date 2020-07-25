247168ea3aa7876be700d49966446c647813a5a8
Mihir Bhansali <mbmihirbhansali@gmail.com>
2020-07-25 22:52:35 +0530

    play store ki to

--- frontend/Frontend/src/CheckUser.js
+++ frontend/Frontend/src/CheckUser.js
@@ -2,32 +2,30 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import ValidDrawer from "../Routes/ValidDrawer";
 import InvalidDrawer from "../Routes/InvalidDrawer";
-import AsyncStorage from '@react-native-community/async-storage';
-import { myAxios, getConfig } from '../../axios';
-import { setUser, signinFailed } from '../../redux/ActionCreators';
-import {View,Image} from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage";
+import { myAxios, getConfig } from "../../axios";
+import { setUser, signinFailed } from "../../redux/ActionCreators";
+import { View, Image } from "react-native";
 
 const CheckUser = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     AsyncStorage.getItem("token")
-      .then(token => {
+      .then((token) => {
         if (token != null) {
           getConfig().then((config) => {
             myAxios
               .get("/profile", config)
               .then((res) => dispatch(setUser(res.data.user, token)))
-              .catch((err) => signinFailed());
+              .catch((err) => dispatch(signinFailed()));
           });
-        }
-        else {
+        } else {
           dispatch(signinFailed());
         }
       })
-      .catch(err => console.log(err));
-  }, [])
+      .catch((err) => dispatch(signinFailed()));
+  }, []);
 
   const user = useSelector((state) => state.user);
   return user.tokenChecking ? (
@@ -46,8 +44,11 @@ const CheckUser = () => {
         style={{ height: 100, width: 100 }}
       />
     </View>
-  )
-    : (user.isValid ? <ValidDrawer /> : <InvalidDrawer />);
+  ) : user.isValid ? (
+    <ValidDrawer />
+  ) : (
+    <InvalidDrawer />
+  );
 };
 
 export default CheckUser;
